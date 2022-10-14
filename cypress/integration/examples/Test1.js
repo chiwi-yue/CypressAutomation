a212418d7d226f78512d6a263514399dbf8f50d4
Chiwi <stanmma@gmail.com>
2022-10-14 15:51:22 +1000

    update

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,19 @@
-// Cypress - Spec
+/// <reference types="Cypress" />
 
 describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(false)
+    it('visit the Kitchen Sink', () => {
+      cy.visit('https://example.cypress.io')
+
+      cy.contains('type').click()
+
+      // Should be on a new URL which
+      // includes '/commands/actions'
+      cy.url().should('include', '/commands/actions')
+
+      // Get an input, type into it and verify
+      // that value has been udpated
+      cy.get('.action-email')
+        .type('fake@gmail.com')
+        .should('have.value', 'fake@gmail.com')
     })
   })
