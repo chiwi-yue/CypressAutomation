24e49eae0d44345f80d7aff07344f51d1d3964b7
Chiwi <stanmma@gmail.com>
2022-11-24 11:45:36 +1000

    fix

---
+++ cypress/integration/examples/dropdown.cy.js
@@ -0,0 +1,12 @@
+// Dropdown practice
+/// <reference types="cypress" />
+
+describe('Dropdown testing practice', () => {
+    beforeEach(() => {
+        cy.visit('https://www.google.com/')
+    });
+
+    it('', () => {
+        
+    });
+})
